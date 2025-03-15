ec572098467a980b6b6487b0b0278f6e59d65f6f
k1ngsterr1 <ruslanmakhmatov@gmail.com>
2025-03-15 14:03:46 +0500

    fix: add error handling & plugin set up for camera

--- App.tsx
+++ App.tsx
@@ -1,97 +1,86 @@
-import { GestureHandlerRootView } from 'react-native-gesture-handler';
-import { SafeAreaProvider } from 'react-native-safe-area-context';
-import { StatusBar, View } from 'react-native';
+import { GestureHandlerRootView } from "react-native-gesture-handler";
+import { SafeAreaProvider } from "react-native-safe-area-context";
+import { StatusBar, View } from "react-native";
 import { useFonts } from "expo-font";
-import { useEffect, useState, useCallback } from 'react';
-import { SplashScreen } from 'expo-router';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import { NavigationContainer } from '@react-navigation/native';
-import { LoaderScreen } from './src/app/screens/loader-screen';
-import { MainScreen } from './src/app/screens/main-screen';
-import { CameraScreen } from './src/app/screens/camera-screen';
-import { DiagnosisScreen } from './src/app/screens/diagnosis-screen';
-import { CityChoiceScreen } from './src/app/screens/city-choice-screen';
-import { DoctorsScreen } from './src/app/screens/doctors-screen';
-import { WebViewScreen } from './src/app/screens/web-view-screen';
+import { useEffect, useState, useCallback } from "react";
+import { SplashScreen } from "expo-router";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { NavigationContainer } from "@react-navigation/native";
+import { LoaderScreen } from "./src/app/screens/loader-screen";
+import { MainScreen } from "./src/app/screens/main-screen";
+import { CameraScreen } from "./src/app/screens/camera-screen";
+import { DiagnosisScreen } from "./src/app/screens/diagnosis-screen";
+import { CityChoiceScreen } from "./src/app/screens/city-choice-screen";
+import { DoctorsScreen } from "./src/app/screens/doctors-screen";
+import { WebViewScreen } from "./src/app/screens/web-view-screen";
 
-
-import './global.css';
-
+import "./global.css";
 
 // Немедленно предотвращаем автоматическое скрытие сплеш-скрина
 SplashScreen.preventAutoHideAsync().catch(() => {
-    /* если возникает ошибка, игнорируем */
+  /* если возникает ошибка, игнорируем */
 });
 
 const Stack = createNativeStackNavigator();
 
 export default function App() {
-    const [fontsLoaded] = useFonts({
-        'ProximaNova-Regular': require('./assets/fonts/Proxima Nova/proximanova_regular.ttf'),
-        'ProximaNova-Bold': require('./assets/fonts/Proxima Nova/proximanova_bold.otf'),
-        'ProximaNova-Black': require('./assets/fonts/Proxima Nova/proximanova_black.ttf'),
-        'ProximaNova-Light': require('./assets/fonts/Proxima Nova/proximanova_light.otf'),
-        'ProximaNova-ExtraBold': require('./assets/fonts/Proxima Nova/proximanova_extrabold.otf'),
-    });
+  const [fontsLoaded] = useFonts({
+    "ProximaNova-Regular": require("./assets/fonts/Proxima Nova/proximanova_regular.ttf"),
+    "ProximaNova-Bold": require("./assets/fonts/Proxima Nova/proximanova_bold.otf"),
+    "ProximaNova-Black": require("./assets/fonts/Proxima Nova/proximanova_black.ttf"),
+    "ProximaNova-Light": require("./assets/fonts/Proxima Nova/proximanova_light.otf"),
+    "ProximaNova-ExtraBold": require("./assets/fonts/Proxima Nova/proximanova_extrabold.otf"),
+  });
 
-    const [isSplashVisible, setIsSplashVisible] = useState(true);
-    const [appIsReady, setAppIsReady] = useState(false);
+  const [isSplashVisible, setIsSplashVisible] = useState(true);
+  const [appIsReady, setAppIsReady] = useState(false);
 
-    // Подготовка приложения к запуску
-    useEffect(() => {
-        async function prepare() {
-            try {
-                // Здесь можно добавить любую дополнительную инициализацию
-                // Например, загрузку данных из AsyncStorage и т.д.
-
-                // Ждем загрузки шрифтов
-                if (fontsLoaded) {
-                    // Когда все готово, отмечаем приложение как готовое
-                    setAppIsReady(true);
-                }
-            } catch (e) {
-                console.warn("Ошибка при подготовке приложения:", e);
-            }
+  // Подготовка приложения к запуску
+  useEffect(() => {
+    async function prepare() {
+      try {
+        if (fontsLoaded) {
+          setAppIsReady(true);
         }
-
-        prepare();
-    }, [fontsLoaded]);
-
-    // Обработчик для скрытия сплеш-скрина
-    const onLayoutRootView = useCallback(async () => {
-        if (appIsReady) {
-            // Скрываем нативный сплеш-скрин только когда приложение полностью готово
-            await SplashScreen.hideAsync();
-        }
-    }, [appIsReady]);
-
-    // Если шрифты не загружены или приложение не готово, показываем пустой экран
-    // Это предотвращает мерцание, так как нативный сплеш-скрин все еще виден
-    if (!appIsReady) {
-        return null;
+      } catch (e) {
+        console.warn("Ошибка при подготовке приложения:", e);
+      }
     }
 
-    return (
-        <GestureHandlerRootView className="flex-1" onLayout={onLayoutRootView}>
-            <SafeAreaProvider>
-                <View className="flex-1 w-full m-auto bg-white">
-                    <StatusBar translucent backgroundColor="transparent" />
-                    {isSplashVisible ? (
-                        <LoaderScreen onAnimationEnd={() => setIsSplashVisible(false)} />
-                    ) : (
-                        <NavigationContainer>
-                            <Stack.Navigator screenOptions={{ headerShown: false }}>
-                                <Stack.Screen name="Main" component={MainScreen} />
-                                <Stack.Screen name="Camera" component={CameraScreen} />
-                                <Stack.Screen name="Diagnosis" component={DiagnosisScreen} />
-                                <Stack.Screen name="CityChoice" component={CityChoiceScreen} />
-                                <Stack.Screen name="Doctors" component={DoctorsScreen} />
-                                <Stack.Screen name="WebView" component={WebViewScreen} />
-                            </Stack.Navigator>
-                        </NavigationContainer>
-                    )}
-                </View>
-            </SafeAreaProvider>
-        </GestureHandlerRootView>
-    );
+    prepare();
+  }, [fontsLoaded]);
+
+  const onLayoutRootView = useCallback(async () => {
+    if (appIsReady) {
+      await SplashScreen.hideAsync();
+    }
+  }, [appIsReady]);
+
+  if (!appIsReady) {
+    return null;
+  }
+
+  return (
+    <GestureHandlerRootView className="flex-1" onLayout={onLayoutRootView}>
+      <SafeAreaProvider>
+        <View className="flex-1 w-full m-auto bg-white">
+          <StatusBar translucent backgroundColor="transparent" />
+          {isSplashVisible ? (
+            <LoaderScreen onAnimationEnd={() => setIsSplashVisible(false)} />
+          ) : (
+            <NavigationContainer>
+              <Stack.Navigator screenOptions={{ headerShown: false }}>
+                <Stack.Screen name="Main" component={MainScreen} />
+                <Stack.Screen name="Camera" component={CameraScreen} />
+                <Stack.Screen name="Diagnosis" component={DiagnosisScreen} />
+                <Stack.Screen name="CityChoice" component={CityChoiceScreen} />
+                <Stack.Screen name="Doctors" component={DoctorsScreen} />
+                <Stack.Screen name="WebView" component={WebViewScreen} />
+              </Stack.Navigator>
+            </NavigationContainer>
+          )}
+        </View>
+      </SafeAreaProvider>
+    </GestureHandlerRootView>
+  );
 }
--- src/app/screens/camera-screen.tsx
+++ src/app/screens/camera-screen.tsx
@@ -1,212 +1,229 @@
-import TakePhotoIcon from '@/src/shared/icons/take-photo-icon';
-import { CameraView, CameraType, useCameraPermissions } from 'expo-camera';
-import { useState, useRef, useEffect } from 'react';
-import { StyleSheet, TouchableOpacity, View, Alert, Image, Animated, SafeAreaView } from 'react-native';
-import { TapGestureHandler } from 'react-native-gesture-handler';
-import { Button } from '@/src/shared/ui/button/button';
+import TakePhotoIcon from "@/src/shared/icons/take-photo-icon";
+import { CameraView, CameraType, useCameraPermissions } from "expo-camera";
+import { useState, useRef, useEffect } from "react";
+import {
+  StyleSheet,
+  TouchableOpacity,
+  View,
+  Alert,
+  Image,
+  Animated,
+  SafeAreaView,
+} from "react-native";
+import { TapGestureHandler } from "react-native-gesture-handler";
+import { Button } from "@/src/shared/ui/button/button";
 
-import Text from '@/src/shared/ui/text/text';
+import Text from "@/src/shared/ui/text/text";
 
-import { useNavigation } from '@react-navigation/native';
-import { uploadPhoto } from '@/src/entities/upload-photo';
-
+import { useNavigation } from "@react-navigation/native";
+import { uploadPhoto } from "@/src/entities/upload-photo";
 
 export const CameraScreen = () => {
-    const [facing, setFacing] = useState<CameraType>('back');
-    const [permission, requestPermission] = useCameraPermissions();
-    const [isLoading, setIsLoading] = useState(false);
-    const cameraRef = useRef<CameraView>(null);
-    const scanLineAnim = useRef(new Animated.Value(0)).current;
+  const [hasPermission, setHasPermission] = useState<boolean | null>(null);
+  const [facing, setFacing] = useState<CameraType>("back");
+  const [permission, requestPermission] = useCameraPermissions();
+  const [isLoading, setIsLoading] = useState(false);
+  const cameraRef = useRef<CameraView>(null);
+  const scanLineAnim = useRef(new Animated.Value(0)).current;
 
-    const { navigate } = useNavigation()
+  const { navigate } = useNavigation();
 
-    useEffect(() => {
-        (async () => {
-            if (!permission || permission.status === "undetermined") {
-                await requestPermission();
-            }
-        })()
-    }, [permission]);
+  useEffect(() => {
+    (async () => {
+      if (!permission || permission.status === "undetermined") {
+        await requestPermission();
+      }
+    })();
+  }, [permission]);
 
-    useEffect(() => {
-        startScanAnimation();
-        return () => {
-            scanLineAnim.stopAnimation();
-        };
-    }, []);
-
-    const startScanAnimation = () => {
-        Animated.loop(
-            Animated.sequence([
-                Animated.timing(scanLineAnim, {
-                    toValue: 1,
-                    duration: 2000,
-                    useNativeDriver: true,
-                }),
-                Animated.timing(scanLineAnim, {
-                    toValue: 0,
-                    duration: 2000,
-                    useNativeDriver: true,
-                }),
-            ])
-        ).start();
+  useEffect(() => {
+    startScanAnimation();
+    return () => {
+      scanLineAnim.stopAnimation();
     };
+  }, []);
 
-    if (!permission) {
-        return <View />;
-    }
+  const startScanAnimation = () => {
+    Animated.loop(
+      Animated.sequence([
+        Animated.timing(scanLineAnim, {
+          toValue: 1,
+          duration: 2000,
+          useNativeDriver: true,
+        }),
+        Animated.timing(scanLineAnim, {
+          toValue: 0,
+          duration: 2000,
+          useNativeDriver: true,
+        }),
+      ])
+    ).start();
+  };
 
-    if (permission.status !== 'granted') {
-        return (
-            <View className='flex flex-col items-center justify-center top-1/2 gap-y-8'>
-                <Text className='text-xl text-black -mt-8'>Использование камеры - обязательно</Text>
-                <Button variant='diagnosis' onPress={requestPermission}>
-                    <Text className='text-white text-[18px]'>Разрешить</Text>
-                </Button>
-            </View>
-        );
-    }
+  if (!permission) {
+    return <View />;
+  }
 
-    function toggleCameraFacing() {
-        setFacing(current => (current === 'back' ? 'front' : 'back'));
-    }
-
-    async function takePhoto() {
-        if (cameraRef.current && !isLoading) {
-            try {
-                setIsLoading(true);
-                const photo = await cameraRef.current.takePictureAsync();
-                console.log("📸 Photo taken:", photo);
-
-                const response = await uploadPhoto(photo);
-                console.log("📤 Upload response:", response);
-
-                navigate("Diagnosis", { photo, diagnosisData: response } as never);
-            } catch (error: any) {
-                console.error("❌ Error:", error);
-                Alert.alert(
-                    "Error",
-                    error.message || "An error occurred while processing your photo"
-                );
-            } finally {
-                setIsLoading(false);
-            }
-        }
-    }
-
-    if (isLoading) {
-        return (
-            <SafeAreaView className="bg-white flex-1 justify-center items-center">
-                <Text weight="regular" className="text-[#8B8B8B] text-[16px]">Анализируем фото...</Text>
-            </SafeAreaView>
-        )
-    }
-
+  if (permission.status !== "granted") {
     return (
-        <View style={styles.container}>
-            <TapGestureHandler numberOfTaps={2} onActivated={toggleCameraFacing}>
-                <CameraView
-                    ref={cameraRef}
-                    style={styles.camera}
-                    facing={facing}
-                >
-                    <View className="absolute inset-0 flex items-center justify-center">
-                        <View className="mb-16 w-[100%] h-[75%] flex items-center justify-center relative">
-                            <Image
-                                source={require('@/assets/images/scan-image.png')}
-                                className="w-full h-full"
-                                resizeMode="contain"
-                            />
-                            <Animated.View
-                                style={[
-                                    styles.scanLine,
-                                    {
-                                        transform: [{
-                                            translateY: scanLineAnim.interpolate({
-                                                inputRange: [0, 1],
-                                                outputRange: [-100, 100]
-                                            })
-                                        }]
-                                    }
-                                ]}
-                            />
-                        </View>
-                    </View>
-                    <View style={styles.buttonContainer}>
-                        <TouchableOpacity
-                            style={[styles.captureButton, isLoading && styles.captureButtonDisabled]}
-                            onPress={takePhoto}
-                            disabled={isLoading}
-                        >
-                            <TakePhotoIcon />
-                        </TouchableOpacity>
-                    </View>
-                </CameraView>
-            </TapGestureHandler>
-        </View>
+      <View className="flex flex-col items-center justify-center top-1/2 gap-y-8">
+        <Text className="text-xl text-black -mt-8">
+          Использование камеры - обязательно
+        </Text>
+        <Button variant="diagnosis" onPress={requestPermission}>
+          <Text className="text-white text-[18px]">Разрешить</Text>
+        </Button>
+      </View>
     );
-}
+  }
+
+  function toggleCameraFacing() {
+    setFacing((current) => (current === "back" ? "front" : "back"));
+  }
+
+  async function takePhoto() {
+    if (cameraRef.current && !isLoading) {
+      try {
+        setIsLoading(true);
+        const photo = await cameraRef.current.takePictureAsync();
+        console.log("📸 Photo taken:", photo);
+
+        const response = await uploadPhoto(photo);
+        console.log("📤 Upload response:", response);
+
+        if (!cameraRef.current) {
+          console.error("🚨 Camera reference is null");
+          Alert.alert("Error", "Camera is not ready. Please try again.");
+          return;
+        }
+
+        navigate("Diagnosis", { photo, diagnosisData: response } as never);
+      } catch (error: any) {
+        console.error("❌ Error:", error);
+        Alert.alert(
+          "Error",
+          error.message || "An error occurred while processing your photo"
+        );
+      } finally {
+        setIsLoading(false);
+      }
+    }
+  }
+
+  if (isLoading) {
+    return (
+      <SafeAreaView className="bg-white flex-1 justify-center items-center">
+        <Text weight="regular" className="text-[#8B8B8B] text-[16px]">
+          Анализируем фото...
+        </Text>
+      </SafeAreaView>
+    );
+  }
+
+  return (
+    <View style={styles.container}>
+      <TapGestureHandler numberOfTaps={2} onActivated={toggleCameraFacing}>
+        <CameraView ref={cameraRef} style={styles.camera} facing={facing}>
+          <View className="absolute inset-0 flex items-center justify-center">
+            <View className="mb-16 w-[100%] h-[75%] flex items-center justify-center relative">
+              <Image
+                source={require("@/assets/images/scan-image.png")}
+                className="w-full h-full"
+                resizeMode="contain"
+              />
+              <Animated.View
+                style={[
+                  styles.scanLine,
+                  {
+                    transform: [
+                      {
+                        translateY: scanLineAnim.interpolate({
+                          inputRange: [0, 1],
+                          outputRange: [-100, 100],
+                        }),
+                      },
+                    ],
+                  },
+                ]}
+              />
+            </View>
+          </View>
+          <View style={styles.buttonContainer}>
+            <TouchableOpacity
+              style={[
+                styles.captureButton,
+                isLoading && styles.captureButtonDisabled,
+              ]}
+              onPress={takePhoto}
+              disabled={isLoading}
+            >
+              <TakePhotoIcon />
+            </TouchableOpacity>
+          </View>
+        </CameraView>
+      </TapGestureHandler>
+    </View>
+  );
+};
 
 const styles = StyleSheet.create({
-    captureButtonDisabled: {
-        opacity: 0.5,
-    },
-    container: {
-        flex: 1,
-        justifyContent: 'center',
-        position: 'relative'
-    },
-    message: {
-        textAlign: 'center',
-        paddingBottom: 10,
-    },
-    camera: {
-        flex: 1,
-    },
-    buttonContainer: {
-        flex: 1,
-        marginTop: 'auto',
-        justifyContent: 'flex-end',
-        backgroundColor: 'transparent',
-        marginBottom: 20
-    },
-    button: {
-        flex: 0.3,
-        alignSelf: 'flex-end',
-        alignItems: 'center',
-    },
-    text: {
-        fontSize: 24,
-        fontWeight: 'bold',
-        color: 'white',
-    },
-    scanLine: {
-        position: 'absolute',
-        height: 4,
-        width: '82%',
-        backgroundColor: 'rgba(255, 255, 255, 0.8)',
-        boxShadow: '0px 2px 12px 0px #FFFFFF',
-        shadowColor: '#FFFFFF',
-        shadowOffset: { width: 0, height: 2 },
-        shadowOpacity: 1,
-        borderRadius: 32,
-        elevation: 5,
-        alignSelf: 'center',
-    },
-    captureButton: {
-        alignSelf: 'center',
-        width: 82,
-        height: 82,
-        borderRadius: 41,
-        backgroundColor: '#F24942',
-        justifyContent: 'center',
-        alignItems: 'center',
-        shadowColor: '#EA3E41',
-        shadowOffset: { width: 0, height: 6 },
-        shadowOpacity: 0.5,
-        shadowRadius: 18,
-        elevation: 10
-    },
+  captureButtonDisabled: {
+    opacity: 0.5,
+  },
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    position: "relative",
+  },
+  message: {
+    textAlign: "center",
+    paddingBottom: 10,
+  },
+  camera: {
+    flex: 1,
+  },
+  buttonContainer: {
+    flex: 1,
+    marginTop: "auto",
+    justifyContent: "flex-end",
+    backgroundColor: "transparent",
+    marginBottom: 20,
+  },
+  button: {
+    flex: 0.3,
+    alignSelf: "flex-end",
+    alignItems: "center",
+  },
+  text: {
+    fontSize: 24,
+    fontWeight: "bold",
+    color: "white",
+  },
+  scanLine: {
+    position: "absolute",
+    height: 4,
+    width: "82%",
+    backgroundColor: "rgba(255, 255, 255, 0.8)",
+    boxShadow: "0px 2px 12px 0px #FFFFFF",
+    shadowColor: "#FFFFFF",
+    shadowOffset: { width: 0, height: 2 },
+    shadowOpacity: 1,
+    borderRadius: 32,
+    elevation: 5,
+    alignSelf: "center",
+  },
+  captureButton: {
+    alignSelf: "center",
+    width: 82,
+    height: 82,
+    borderRadius: 41,
+    backgroundColor: "#F24942",
+    justifyContent: "center",
+    alignItems: "center",
+    shadowColor: "#EA3E41",
+    shadowOffset: { width: 0, height: 6 },
+    shadowOpacity: 0.5,
+    shadowRadius: 18,
+    elevation: 10,
+  },
 });
-
-
